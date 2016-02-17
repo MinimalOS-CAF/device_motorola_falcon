8744513d0e7b8a462b57132d7504d65c982f616f
Aayushya <aayushyaaankul@gmail.com>
2016-02-17 09:42:59 -0800

    Minimal-ify

--- minimal.mk
+++ minimal.mk
@@ -1,10 +1,10 @@
-# Inherit some common Orion stuff.
-$(call inherit-product, vendor/orion/config/common_full_phone.mk)
+# Inherit some common Minimal stuff.
+$(call inherit-product, vendor/minimal/config/common_full_phone.mk)
 
 # Inherit device specific
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := orion_falcon
+PRODUCT_NAME := minimal_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
